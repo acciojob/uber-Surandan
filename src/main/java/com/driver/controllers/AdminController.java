9792ce8af3a7a86f8b0a291412c4c1bf79cfee8d
Surandan <110124420+Surandan@users.noreply.github.com>
2023-08-01 19:10:54 +0530

    Exceptions thrown

--- src/main/java/com/driver/controllers/AdminController.java
+++ src/main/java/com/driver/controllers/AdminController.java
@@ -26,7 +26,7 @@ public class AdminController {
 	}
 
 	@PutMapping("/update")
-	public ResponseEntity<Admin> updateAdminPassword(@RequestParam Integer adminId, @RequestParam String password){
+	public ResponseEntity<Admin> updateAdminPassword(@RequestParam Integer adminId, @RequestParam String password) throws Exception{
 		try{
 			Admin updatedAdmin = adminService.updatePassword(adminId,password);
 			return new ResponseEntity<>(updatedAdmin, HttpStatus.OK);
